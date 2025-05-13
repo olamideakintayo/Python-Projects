31cf9e926fcf1ac364f64030e8004db34382f46a
olamideakintayo <divineolamide977@gmail.com>
2025-05-13 05:12:56 +0100

    initial commit with python statistics function

---
+++ chapter_3_tasks/mean_median_and_mode.py
@@ -0,0 +1,13 @@
+#mean_median_and_mode.py
+
+import statistics
+
+values = [9,11,22,34,17,22,34,22,40]
+
+mean_value = statistics.mean(values)
+median_value = statistics.median(values)
+mode_value = statistics.mode(values)
+
+print(mean_value)
+print(median_value)
+print(mode_value)
