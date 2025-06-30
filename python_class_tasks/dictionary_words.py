2d22afddfebc52ed7063567bf8f41e443b2db152
olamideakintayo <divineolamide977@gmail.com>
2025-06-30 15:19:35 +0100

    updating the logic

--- python_class_tasks/dictionary_words.py
+++ python_class_tasks/dictionary_words.py
@@ -1,12 +1,5 @@
 def count_letters(word):
-    letter_count = {}
-    for letter in word:
-        if letter in letter_count:
-            letter_count[letter] = letter_count[letter] + 1
-        else:
-            letter_count[letter] = 1
-    return letter_count
-
+    return {letter: word.count(letter) for letter in word}
 
 word = input("Enter a word: ")
 print(count_letters(word))
